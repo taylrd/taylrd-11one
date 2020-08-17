19aff5bb9b122b0c3a33a5162263cdd6542f800e
Corey Taylor <corey@taylrd.com>
2020-08-17 16:37:44 -0400

    wip: 2nd round of acct conf template for email

--- .eleventy.js
+++ .eleventy.js
@@ -1,53 +1,48 @@
-
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(config) {
+	// A useful way to reference the context we are runing eleventy in
+	let env = process.env.ELEVENTY_ENV;
 
-  // A useful way to reference the context we are runing eleventy in
-  let env = process.env.ELEVENTY_ENV;
+	// Layout aliases can make templates more portable
+	config.addLayoutAlias('default', 'layouts/base.njk');
 
-  // Layout aliases can make templates more portable
-  config.addLayoutAlias('default', 'layouts/base.njk');
+	// Add some utility filters
+	config.addFilter('squash', require('./src/utils/filters/squash.js'));
+	config.addFilter('dateDisplay', require('./src/utils/filters/date.js'));
 
-  // Add some utility filters
-  config.addFilter("squash", require("./src/utils/filters/squash.js") );
-  config.addFilter("dateDisplay", require("./src/utils/filters/date.js") );
+	// add support for syntax highlighting
+	config.addPlugin(syntaxHighlight);
 
+	// minify the html output
+	config.addTransform('htmlmin', require('./src/utils/minify-html.js'));
 
-  // add support for syntax highlighting
-  config.addPlugin(syntaxHighlight);
+	// compress and combine js files
+	config.addFilter('jsmin', function(code) {
+		const UglifyJS = require('uglify-js');
+		let minified = UglifyJS.minify(code);
+		if (minified.error) {
+			console.log('UglifyJS error: ', minified.error);
+			return code;
+		}
+		return minified.code;
+	});
 
-  // minify the html output
-  config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
+	// pass some assets right through
+	config.addPassthroughCopy('./src/site/images');
+	config.addPassthroughCopy('./src/site/acct-conf.html');
 
-  // compress and combine js files
-  config.addFilter("jsmin", function(code) {
-    const UglifyJS = require("uglify-js");
-    let minified = UglifyJS.minify(code);
-      if( minified.error ) {
-          console.log("UglifyJS error: ", minified.error);
-          return code;
-      }
-      return minified.code;
-  });
-
-
-  // pass some assets right through
-  config.addPassthroughCopy("./src/site/images");
-
-  // make the seed target act like prod
-  env = (env=="seed") ? "prod" : env;
-  return {
-    dir: {
-      input: "src/site",
-      output: "dist",
-      data: `_data/${env}`
-    },
-    templateFormats : ["njk", "md", "11ty.js"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
-    passthroughFileCopy: true
-  };
-
+	// make the seed target act like prod
+	env = env == 'seed' ? 'prod' : env;
+	return {
+		dir                    : {
+			input  : 'src/site',
+			output : 'dist',
+			data   : `_data/${env}`
+		},
+		templateFormats        : [ 'njk', 'md', '11ty.js' ],
+		htmlTemplateEngine     : 'njk',
+		markdownTemplateEngine : 'njk',
+		passthroughFileCopy    : true
+	};
 };
